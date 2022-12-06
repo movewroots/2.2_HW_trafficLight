51a9f619d55ad25c84a94f62fede7c49ea3b61d8
movewroots <movinonwithroots@gmail.com>
2022-12-06 23:15:37 +0300

    Initial Commit

---
+++ 2.2_HW_trafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.2_HW_trafficLight
+//
+//  Created by Nikita Verkhoturov on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
